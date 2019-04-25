fbd59b7a7ef0e805446298b9ecc05a31cab35e6f
EryouHao <haoeryou@qq.com>
2019-04-25 13:19:10 +0800

    feat: add theme new link

--- .github/issue_template.md
+++ .github/issue_template.md
@@ -3,17 +3,17 @@ title: 主题推荐
 labels: 主题
 ---
 
-## 主题名称
+## 主题名称 / Theme Name
 - 
 
-## 介绍
+## 介绍 / Introduction
 - 
 
-## 仓库地址
+## 仓库地址 / Github Repo
 - 
 
-## 预览地址
+## 预览地址 / Preview Address
 -
 
-## 截图
+## 截图 / Screenshots
 -
--- .vuepress/components/ThemeMarket/Index.vue
+++ .vuepress/components/ThemeMarket/Index.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="theme-market sm:px-1">
+    <div class="text-center lg:px-4 pb-10">
+      <div class="p-2 bg-yellow items-center text-yellow-darkest leading-none lg:rounded-full flex lg:inline-flex cursor-pointer" role="alert" @click="newTheme">
+        <span class="flex rounded-full bg-yellow-dark uppercase px-2 py-1 text-xs font-bold mr-3">Click Here</span>
+        <span class="font-semibold mr-2 text-left flex-auto">快来将你创作的主题分享给我们吧！</span>
+        <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+      </div>
+    </div>
     <l-row :gutter="24">
       <l-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8" v-for="(item, index) in list" :key="index">
         <div class="theme-item shadow-md rounded bg-white hover:shadow-lg" :style="{ 'background-image': `url(${item.image})` }" @click="download(item)">
@@ -48,6 +55,9 @@ export default {
   methods: {
     download(item) {
       window.open(item.github)
+    },
+    newTheme() {
+      window.open('https://github.com/getgridea/site/issues/new')
     },
   },
 }
